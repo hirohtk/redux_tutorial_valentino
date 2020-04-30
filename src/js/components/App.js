53e3b20566a00e5a8ac5bcd64886473575b01314
Kensen <hirohtk@gmail.com>
2020-04-29 19:49:01 -0700

    functioning (very basic) app with adding a string to store with form and list component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import List from "./List";
+import Form from "./Form";
 
 const App = () => (
-  <div>
-    <h2>Articles</h2>
-      <List />
-  </div>
+    <React.Fragment>
+        <div>
+            <h2>Articles</h2>
+            <List />
+        </div>
+        <div>
+            <h2>Add a new article</h2>
+            <Form />
+        </div>
+    </React.Fragment>
 );
 
 export default App;
